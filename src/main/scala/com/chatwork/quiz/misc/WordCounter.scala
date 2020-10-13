1ad4319effc208998ff7a61a4e574e165f2d3aaa
ymgn <gonyamagon@gmail.com>
2020-10-14 00:22:14 +0900

    add 問題1を実装した

--- src/main/scala/com/chatwork/quiz/misc/WordCounter.scala
+++ src/main/scala/com/chatwork/quiz/misc/WordCounter.scala
@@ -11,6 +11,18 @@ object WordCounter {
     * @param words 文字列
     * @return 単語がキー、単語数がヴァリューのマップ
     */
-  def countWords(words: List[String]): Map[String, Int] = ???
+  def countWords(words: List[String]): Map[String, Int] = {
+//    Map(
+//      "banana" -> 1,
+//      "muscat" -> 1,
+//      "orange" -> 3,
+//      "mango"  -> 2,
+//      "apple"  -> 3,
+//      "kiwi"   -> 1,
+//      "papaya" -> 1
+//    )
+    words.flatMap(f=>f.split(" ")).groupBy(identity).view.mapValues(_.size).toMap
+
+  }
 
 }
